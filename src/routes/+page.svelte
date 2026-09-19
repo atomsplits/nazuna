f357dad73b1273c0ccbcf99e26f5dd9c0177a3dd
roundhouses <1282053p@gmail.com>
2026-09-19 22:41:37 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<div class="w-screen h-screen justify-center items-center flex flex-col">
+  <div class="justify-center items-center flex flex-col">
+    <h1 class="text-2xl text-white">nazuna!</h1>
+    <p class="text-md text-white">atomsplits' homepage (wip)</p>
+  </div>
+
+  <div
+    class="justify-center items-center flex flex-row gap-5 text-white text-md mt-5"
+  >
+    <button class="pl-5 pr-5 p-2 border-2 border-white rounded-md"
+      >about me</button
+    >
+    <button class="pl-5 pr-5 p-2 border-2 border-white rounded-md"
+      >projects</button
+    >
+    <button class="pl-5 pr-5 p-2 border-2 border-white rounded-md"
+      >commission</button
+    >
+  </div>
+</div>
+
+<style>
+  :root {
+    background-color: black;
+  }
+</style>
