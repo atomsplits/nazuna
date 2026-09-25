d749e07a1c24b9898b809bef1078880273e95d0f
roundhouses <1282053p@gmail.com>
2026-09-25 16:29:40 +0100

    added some more stuff and an about me page (wip)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,26 @@
 <script>
   import "./scripts/three"
+  import pfp from "$lib/assets/image.jpg"
 </script>
 
 <div class="bg-black w-screen h-screen z-10 absolute opacity-60"></div>
 <div class="w-screen h-screen justify-center items-center flex flex-col absolute z-20">
     <div class="justify-center items-center flex flex-col">
-        <h1 class="text-2xl text-white">nazuna!</h1>
+        <img src={pfp} alt="" class="max-w-32 max-h-32 rounded-full m-5 hover:scale-110 duration-300 hover:blur-sm border-4 border-neutral-300">
+        <h1 class="text-2xl text-white font-extralight hover:scale-125 duration-300">nazuna!</h1>
         <p class="text-md text-white">atomsplits' homepage (wip)</p>
     </div>
 
     <div
         class="justify-center items-center flex flex-row gap-5 text-white text-md mt-5"
     >
-        <button class="pl-5 pr-5 p-2 border-2 border-white rounded-md"
+        <button onclick={() => { window.location.href = '/me' }} class="pl-5 pr-5 p-2 border-2 border-white rounded-md hover:scale-110 hover:bg-white hover:text-black duration-150"
             >about me</button
         >
-        <button class="pl-5 pr-5 p-2 border-2 border-white rounded-md"
+        <button class="pl-5 pr-5 p-2 border-2 border-white rounded-md hover:scale-110 hover:bg-white hover:text-black duration-150"
             >projects</button
         >
-        <button class="pl-5 pr-5 p-2 border-2 border-white rounded-md"
+        <button class="pl-5 pr-5 p-2 border-2 border-white rounded-md hover:scale-110 hover:bg-white hover:text-black duration-150"
             >commission</button
         >
     </div>
---
+++ src/routes/me/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+    import pfp from '$lib/assets/image.jpg'
+</script>
+
+<div class="w-full h-1/2 justify-center items-center flex flex-row p-5 gap-10">
+<div class="flex flex-col gap-5 items-center justify-center bg-neutral-900 p-5 rounded-xl">
+    <img src={pfp} alt="" class="w-72 h-72 rounded-full border-2 border-neutral-400">
+    <div class="flex flex-row gap-5">
+        <h1 class="text-white font-bold text-2xl">atomsplits</h1>
+        <h1 class="text-white font-extralight text-2xl opacity-70">he/him/they/them</h1>
+    </div>
+    <h1 class="text-white text-xl">nazuna.org</h1>
+</div>
+    <div class="w-1/2 bg-neutral-800 p-5 text-white border-2 border-neutral-600 justify-center flex flex-col rounded-2xl">
+        <p class="font-extralight pb-5">atomsplits / README.md</p>
+        <h1 class="text-xl pb-4">whoami?</h1>
+        <div class="bg-neutral-700 p-2 rounded-xl">
+            <pre class="text-xs whitespace-pre-wrap hover:scale-105 hover:ring-2 hover:ring-cyan-200 duration-300 rounded-2xl">
+                <code>
+                    __          _                            _ _ _
+                    \ \    __ _| |_ ___  _ __ ___  ___ _ __ | (_) |_ ___
+                     \ \  / _` | __/ _ \| '_ ` _ \/ __| '_ \| | | __/ __|
+                     / / | (_| | || (_) | | | | | \__ \ |_) | | | |_\__ \
+                    /_/   \__,_|\__\___/|_| |_| |_|___/ .__/|_|_|\__|___/
+                                                      |_|
+                     .-"""-.    |~> whoami
+                    '        \                      > 18
+                    |,.  ,-.  |                     > russian
+                    |()L( ()| |                     > student @ 42 Lisbon
+                    |,'  `".| |                     > #1 deftones fan
+                    |.___.',| `                     > former passionate distro hopper
+                   .j `--"' `  `.                   > former minecraft hacked client dev
+                  / '        '   \                  > commissioning website coming soon
+                 / /          `   `.                > chronic insomniac
+                / /            `    .               > really bad street photographer
+               / /              l   |               > t.me/atomsplits
+              . ,               |   |               > yt: @atomsplits (devlogs maybe)
+              ,"`.             .|   |               > extras:
+           _.'   ``.          | `..-'l                     > video editor / MC montage creator
+          |       `.`,        |      `.                    > guitar player of 2+ years
+          |         `.    __.j         )                   > skateboarder of 8+ years
+          |__        |--""___|      ,-'                    > x: @atomsplits (i dont really post)
+          `"--...,+""""   `._,.-'                > current tech stack:
+          > editors: neovim/zed/vscode
+          > langs: C/C++/C#/Rust/Python/JS/TS/Java/Kotlin
+          > frameworks/libs: Nuklear/Tauri/Svelte/iced.rs/GTK4/React
+                </code>
+            </pre>
+        </div>
+    </div>
+</div>
+
+<style>
+    :root {
+        background-color: #111111;
+        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='80' height='80' viewBox='0 0 80 80'%3E%3Cg fill='%23000000' fill-opacity='0.4'%3E%3Cpath fill-rule='evenodd' d='M0 0h40v40H0V0zm40 40h40v40H40V40zm0-40h2l-2 2V0zm0 4l4-4h2l-6 6V4zm0 4l8-8h2L40 10V8zm0 4L52 0h2L40 14v-2zm0 4L56 0h2L40 18v-2zm0 4L60 0h2L40 22v-2zm0 4L64 0h2L40 26v-2zm0 4L68 0h2L40 30v-2zm0 4L72 0h2L40 34v-2zm0 4L76 0h2L40 38v-2zm0 4L80 0v2L42 40h-2zm4 0L80 4v2L46 40h-2zm4 0L80 8v2L50 40h-2zm4 0l28-28v2L54 40h-2zm4 0l24-24v2L58 40h-2zm4 0l20-20v2L62 40h-2zm4 0l16-16v2L66 40h-2zm4 0l12-12v2L70 40h-2zm4 0l8-8v2l-6 6h-2zm4 0l4-4v2l-2 2h-2z'/%3E%3C/g%3E%3C/svg%3E");
+    }
+</style>
